f9680eb7857b6c4ea5367457d2493e423b677458
mikyungBae <gg5085@naver.com>
2016-07-14 21:19:20 +0900

    model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709070756) do
+ActiveRecord::Schema.define(version: 20160714121821) do
+
+  create_table "answer_longs", force: :cascade do |t|
+    t.integer  "user_id_id"
+    t.integer  "question_id_id"
+    t.text     "answer"
+    t.integer  "public_result"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "answer_longs", ["question_id_id"], name: "index_answer_longs_on_question_id_id"
+  add_index "answer_longs", ["user_id_id"], name: "index_answer_longs_on_user_id_id"
+
+  create_table "answer_shorts", force: :cascade do |t|
+    t.integer  "user_id_id"
+    t.integer  "question_id_id"
+    t.integer  "answer"
+    t.integer  "public_result"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "answer_shorts", ["question_id_id"], name: "index_answer_shorts_on_question_id_id"
+  add_index "answer_shorts", ["user_id_id"], name: "index_answer_shorts_on_user_id_id"
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -22,6 +46,22 @@ ActiveRecord::Schema.define(version: 20160709070756) do
   end
 
   add_index "comments", ["wall_id"], name: "index_comments_on_wall_id"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "content"
+    t.string   "selection_option"
+    t.integer  "skill_area"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "walls", force: :cascade do |t|
     t.string   "title"
